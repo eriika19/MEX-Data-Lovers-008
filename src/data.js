b8aa5b0c15aa69ea78e13abf55207c9781a66b34
eriika19 <51388362+eriika19@users.noreply.github.com>
2019-07-02 18:52:40 -0500

    se arreglo todo lo que pasooooo

--- src/data.js
+++ src/data.js
@@ -54,9 +54,6 @@ window.handleData = {
 }
 
 
-
-
-
 /*
 //esta es una función de ejemplo
 //puedes ver como agregamos la función a nuestro objeto global window
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-const arr = STEAM.appnews.newsitems
+
+
+
+
+/*const arr = STEAM.appnews.newsitems
 const root = document.getElementById('root')
 arr.forEach(element => {
     console.log(element.feedname)
    root.insertAdjacentHTML("beforeEnd",  `<p>${element.feedname}</p>`)
-});
+});*/
