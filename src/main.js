4dd541d82445b2f0f6570f5cf565d76b012bf522
eriika19 <51388362+eriika19@users.noreply.github.com>
2019-07-01 19:27:33 -0500

    se agrego pocas lineas de codigo en index.html y steam.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const arr = STEAM.appnews.newsitems
+const root = document.getElementById('root')
+arr.forEach(element => {
+    console.log(element.feedname)
+   root.insertAdjacentHTML("beforeEnd",  `<p>${element.feedname}</p>`)
+});
